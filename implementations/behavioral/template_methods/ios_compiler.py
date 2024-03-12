c7f25d4049b7f69b83816593346816b7aa7c40f3
l3co <l3co@outlook.com>
2024-03-12 04:19:58 -0300

    feat(template_method): example of ios compiler

--- implementations/behavioral/template_methods/ios_compiler.py
+++ implementations/behavioral/template_methods/ios_compiler.py
@@ -15,3 +15,19 @@ class Compiler(ABC):
         self.collect_source()
         self.compile_to_object()
         self.run()
+
+
+class IOsCompiler(Compiler):
+    def collect_source(self):
+        print("Collecting swift source code")
+
+    def compile_to_object(self):
+        print("Compiling swift code to LLVM bitcode")
+
+    def run(self):
+        print("Program running on runtime environment")
+
+
+if __name__ == '__main__':
+    ios_compiler = IOsCompiler()
+    ios_compiler.compile_and_run()
